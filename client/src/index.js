93512e5bf9d06a2a865c3fa5e253217e4638c984
dbelett98 <dbelett@outlook.com>
2025-08-08 13:21:12 -0500

    hh

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-// No reportWebVitals import or call (free removal to fix error).
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
